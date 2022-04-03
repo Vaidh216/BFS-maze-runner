3f6aa410888960b051bfaea77a5a5b579855d26a
Vaidh <vedprakashsingh216@gmail.com>
2022-04-03 18:55:30 +0530

    completed this projects next versions will me made as a seprate projects on web

--- path_finder.py
+++ path_finder.py
@@ -2,9 +2,7 @@ import curses
 from curses import wrapper
 import queue
 import time
-
 from sklearn import neighbors
-
 maze = [
     [" ", " ", " ", "#", "#", "O", "#", "#", "#", "#"],
     [" ", "#", " ", " ", " ", " ", " ", " ", " ", "#"],
@@ -12,9 +10,9 @@ maze = [
     [" ", " ", "#", "#", "#", " ", "#", " ", "#", "#"],
     ["#", " ", " ", " ", "#", "#", "#", " ", "#", "#"],
     ["#", " ", "#", "#", " ", " ", "#", " ", " ", " "],
-    ["#", " ", "#", "#", "#", "#", "#", " ", "#", " "],
+    ["#", "#", "#", "#", "#", "#", "#", " ", "#", " "],
     ["#", "X", "#", " ", " ", " ", " ", " ", "#", " "],
-    ["#", " ", " ", " ", "#", "#", "#", " ", " ", " "],
+    ["#", "#", " ", " ", "#", "#", "#", " ", " ", " "],
     ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
 ]
 
@@ -22,9 +20,7 @@ def print_maze(maze,stdscr, path, way):
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
     GREEN = curses.color_pair(3)
-
     stdscr.addstr(0,0,f"Searching Via {way} Alogorithms", GREEN)
-
     for i, row in enumerate(maze):
         for j, value in enumerate(row):
             if (i,j) in path:
@@ -41,6 +37,7 @@ def find_start(maze,start):
     return None;
 
 def find_path_BFS(maze, stdscr):
+    YELLOW = curses.color_pair(4)
     start = "O"
     end = "X"
     start_pos = find_start(maze,start)
@@ -74,8 +71,11 @@ def find_path_BFS(maze, stdscr):
             new_path = path + [neighbor]
             q.put((neighbor,new_path))
             visited.add(neighbor)
+    
+    stdscr.addstr(len(maze)+4,0,"NO path found",YELLOW)
             
 def find_path_DFS(maze,stdscr):
+    YELLOW = curses.color_pair(4)
     start = "O"
     end = "X"
     start_pos = find_start(maze,start)
@@ -109,6 +109,7 @@ def find_path_DFS(maze,stdscr):
             stack.append((neighbor,new_path))
             visited.add(neighbor)
 
+    stdscr.addstr(len(maze)+4,0,"NO path found",YELLOW)
 
 
 def find_neighbors(maze, row, col):
@@ -128,12 +129,11 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     
     find_path_DFS(maze,stdscr)
     stdscr.getch()
-    # stdscr.clear()
-    # print_maze(maze,stdscr) 
-    # stdscr.refresh()
-    # stdscr.getch()
-
+    find_path_BFS(maze,stdscr)
+    stdscr.getch()
+    
 wrapper(main)
